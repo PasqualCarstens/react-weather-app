45fbecaa94a423ea03b11a16713bfd6e57cf4f54
PasqualCarstens <pasqualcarstens@gmail.com>
2021-10-17 16:57:39 +1100

    Added the GitHub repository URL

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App </h1>
+      <footer>
+        <a
+          href="https://github.com/PasqualCarstens/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code
+        </a>{" "}
+        by Pasqual Carstens
+      </footer>
     </div>
   );
 }
